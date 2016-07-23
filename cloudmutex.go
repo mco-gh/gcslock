169440e04fbcc4c0af87286c1477bad4759a5656
Marc Cohen <marcacohen@gmail.com>
2016-07-23 12:26:59 +0100

    add support for local and global mutex using an interface and two structs as receivers for common methods

--- cloudmutex.go
+++ cloudmutex.go
@@ -1,11 +1,11 @@
 package cloudmutex
 
 import (
+	"errors"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	storage "google.golang.org/api/storage/v1"
 	"log"
-	"net/http"
 	"os"
 	"sync"
 )
@@ -18,39 +18,61 @@ const (
 	bucketName = "bucket"
 )
 
-var (
-	mutex                    = &sync.Mutex{}
-	client  *http.Client     = nil
-	service *storage.Service = nil
-	err     error            = nil
-)
-
-func init() {
-	client, err = google.DefaultClient(context.Background(), scope)
-	if err != nil {
-		log.Fatalf("Unable to get default client: %v", err)
-	}
-	service, err = storage.New(client)
-	if err != nil {
-		log.Fatalf("Unable to create storage service: %v", err)
-	}
+type cloudmutex interface {
+	Lock()
+	Unlock()
 }
 
-func Lock() {
-	//mutex.Lock()
+type localmutex struct {
+	mutex *sync.Mutex
+}
+
+type globalmutex struct {
+	service *storage.Service
+}
+
+func (m localmutex) Lock() {
+	m.Lock()
+}
+
+func (m localmutex) Unlock() {
+	m.Unlock()
+}
+
+func (m globalmutex) Lock() {
 	object := &storage.Object{Name: objectName}
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalf("Error opening %q: %v", fileName, err)
 	}
-	if _, err := service.Objects.Insert(bucketName, object).Media(file).Do(); err != nil {
+	if _, err := m.service.Objects.Insert(bucketName, object).Media(file).Do(); err != nil {
 		log.Fatalf("Objects.Insert failed: %v", err)
 	}
 }
 
-func Unlock() {
-	//mutex.Unlock()
-	if err := service.Objects.Delete(bucketName, objectName).Do(); err != nil {
+func (m globalmutex) Unlock() {
+	if err := m.service.Objects.Delete(bucketName, objectName).Do(); err != nil {
 		log.Fatalf("Could not delete object: %v\n", err)
 	}
+}
+
+func newMutex(scope string) (cloudmutex, error) {
+	if scope == "local" {
+		p := new(localmutex)
+		p.mutex = &sync.Mutex{}
+		return p, nil
+	} else if scope == "" || scope == "global" {
+		p := new(globalmutex)
+		client, err := google.DefaultClient(context.Background(), scope)
+		if err != nil {
+			log.Fatalf("Unable to get default client: %v", err)
+		}
+		service, err := storage.New(client)
+		if err != nil {
+			log.Fatalf("Unable to create storage service: %v", err)
+		}
+		p.service = service
+		return p, nil
+	}
+	return nil, errors.New("invalid scope argument: " + scope)
 }
