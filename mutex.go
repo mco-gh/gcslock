6711cec83a6812c3d54a8c34388fa454a5bed422
Marc Cohen <marcacohen@gmail.com>
2016-08-03 10:33:42 +0100

    pkg doc fixed

--- mutex.go
+++ mutex.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// gcslock is a scalable, distributed mutex that can be used to 
-// serialize computations anywhere on the global internet. 
+// Package gcslock is a scalable, distributed mutex that can be used
+// to serialize computations anywhere on the global internet.
 package gcslock
 
 import (
